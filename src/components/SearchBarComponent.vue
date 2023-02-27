2fca8fe68a4ddb68dfe6f8bb8f2d3f89c689f328
BaptisteHouques <baptistehouques38@gmail.com>
2023-02-27 16:42:39 +0100

    search function

--- src/components/SearchBarComponent.vue
+++ src/components/SearchBarComponent.vue
@@ -1,11 +1,46 @@
 <template>
-la barre de recherche
+  <input v-model="query">
+  <button v-on:click="search">button</button>
 </template>
 
-<script>
-export default {
-  name: "SearchBarComponent"
-}
+<script lang="ts">
+import axios, {AxiosError} from "axios";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SearchBarComponent",
+  data () {
+    return {
+      query: ""
+    }
+  },
+  emits: ["emitResults"],
+  methods: {
+    search() {
+      axios.get(import.meta.env.VITE_API_URL + "searc" + "?query=" + this.query + "&pageSize=1" + "&" + import.meta.env.VITE_API_KEY)
+          .then((response) => {
+            this.$emit("emitResults", response.data.foods)
+          })
+          .catch((error) => {
+            console.log(error)
+            this.showError(error)
+          });
+    },
+    showError(error: AxiosError) {
+      if (error.response) {
+        switch (error.response.status){
+          case 400:
+            console.log("erreur dans la requête")
+                break
+          case 500:
+            console.log('erreur server')
+                break
+        }
+
+      }
+    }
+  }
+})
 </script>
 
 <style scoped>
