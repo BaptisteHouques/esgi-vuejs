904b4a36d70ab65bbe35ba70c059685b0112058b
BaptisteHouques <baptistehouques38@gmail.com>
2023-03-03 11:14:00 +0100

    finish cache for product detail

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -6,7 +6,7 @@
     <v-card-item>
       <u>Nutriments:</u>
       <div v-for="nutriment in product.foodNutrients" v-bind:key="nutriment">
-        {{ nutriment.nutrient.name }}:  {{ nutriment.nutrient.number }} {{ nutriment.nutrient.unitName }}
+        {{ nutriment.nutrientName }}:  {{ nutriment.nutrientNumber }} {{ nutriment.unitName }}
       </div>
     </v-card-item>
   </v-card>
@@ -16,38 +16,74 @@
 import axios from "axios";
 import { defineComponent } from "vue";
 import { listFoodStore } from '@/stores/listFood'
+import type ProductInterface from "@/interfaces/ProductInterface";
 
 export default defineComponent({
   name: "ProductDetail.vue",
   data() {
     return {
-      product: []
+      product: {} as ProductInterface
     }
   },
   mounted() {
     listFoodStore().setIsLoading(true)
-    // let isInCache = false
-    //
-    // listFoodStore().searchCache.forEach(search => {
-    //   search.results.forEach(result => {
-    //     if (result.id === parseInt(this.$route.params.id)) {
-    //       this.product = result
-    //       listFoodStore().setIsLoading(false)
-    //     }
-    //   })
-    // })
+    let isInCache = false
 
-    // if (!isInCache) {
+    listFoodStore().searchCache.forEach(search => {
+      search.results.forEach(result => {
+        if (result.fdcId.toString() === this.$route.params.id) {
+          this.product = result
+          console.log('en cache')
+          isInCache = true
+          listFoodStore().setIsLoading(false)
+        }
+      })
+    })
+
+    if (!isInCache) {
+      console.log('pas en cache')
       axios.get(import.meta.env.VITE_API_URL + "food/" + this.$route.params.id + "?" + import.meta.env.VITE_API_KEY)
-        .then(response => {
-          this.product = response.data
+        .then((response) => {
+          //Product recovered by this route doesn't have the same properties as ProductInterface
+          //So in TS we have to define this response
+          let tempProduct: {
+          fdcId: number,
+          description: string,
+          foodNutrients: {
+            nutrient: {
+              name: string,
+              number: number,
+              unitName: string,
+              rank: number
+            }
+          }[]
+          } = response.data
+          // Init a temporary list of Nutrients
+          let tempFoodNutrients: {
+            nutrientName: string,
+            nutrientNumber: number,
+            unitName: string
+          }[] = []
+
+          // Assign the response properties to the corresponding product properties
+          this.product.fdcId = tempProduct.fdcId
+          this.product.description = tempProduct.description
+
+          // Foreach foodNutrient, push response nutrient to the temporary list
+          tempProduct.foodNutrients.forEach((foodNutrient) => {
+            tempFoodNutrients.push({nutrientName: foodNutrient.nutrient.name, nutrientNumber: foodNutrient.nutrient.number, unitName: foodNutrient.nutrient.unitName})
+            if (foodNutrient.nutrient.name === "Energy")
+              this.product.cal = foodNutrient.nutrient.rank
+          })
+          // Assign the temporary list of foodNutrients to the product list
+          this.product.foodNutrients = tempFoodNutrients
           listFoodStore().setIsLoading(false)
         })
         .catch(error => {
           console.log(error)
           listFoodStore().setIsLoading(false)
         })
-    // }
+    }
   },
   computed: {
     getLoadingValue() {
